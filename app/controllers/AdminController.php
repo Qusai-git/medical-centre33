0f0ab04b176cb0e46662cfd2dae103bd9651f854
Qusai-git <zxzsscvbw@gmail.com>
2024-07-07 00:53:09 +0300

    Update AdminController.php

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,19 +1,1 @@
-<?php
-namespace app\controllers;
 
-echo "Hello<br>";
-
-require __DIR__.'/../models/AdminModel.php';
-use app\models\AdminModel;
-
-
-class AdminController {
-    private $Model;
-    
-    public function __construct($db) {
-      
-        $this->model = new AdminModel($db);
-    }
-}
-
-?>
